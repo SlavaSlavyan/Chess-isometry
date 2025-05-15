ef025355cd35b59461fea51925c72492df0af2ce
Svyatoslav <tortik2000.g@gmail.com>
2025-05-15 15:35:15 +0300

    0.2.7

--- function/Game.py
+++ function/Game.py
@@ -1,14 +1,56 @@
 import pygame
 import math
 
+class PieseMove:
+        
+    def __init__(self,m):
+        pass
+    
+    def main(self,m,x:int,y:int):
+        
+        if m.PI.Game.cells[x][y]['value'][6:] == "pawn":
+            self.pawn(m,x,y,m.PI.Game.cells[x][y]['value'][:5])
+            
+    def pawn(self,m,x:int,y:int,team:str):
+        
+        if team == 'white':
+            if y+1<8:
+                if m.PI.Game.cells[x][y+1]['value'] == "empty":
+                    m.PI.Game.cells[x][y+1]['status'] = 'move'
+                    if y+2<8:
+                        if y == 1:
+                            m.PI.Game.cells[x][y+2]['status'] = 'move'
+                if x-1>-1:
+                    if m.PI.Game.cells[x-1][y+1]['value'][:5] == "black":
+                        m.PI.Game.cells[x-1][y+1]['status'] = 'attack'
+                if x+1<8:
+                    if m.PI.Game.cells[x+1][y+1]['value'][:5] == "black":
+                        m.PI.Game.cells[x+1][y+1]['status'] = 'attack'
+                        
+        if team == 'black':
+            if x-1>-1:
+                if m.PI.Game.cells[x][y-1]['value'] == "empty":
+                    m.PI.Game.cells[x][y-1]['status'] = 'move'
+                    if y-2>-1:
+                        if y == 6:
+                            m.PI.Game.cells[x][y-2]['status'] = 'move'
+                if x+1<8:
+                    if m.PI.Game.cells[x+1][y-1]['value'][:5] == "white":
+                        m.PI.Game.cells[x+1][y-1]['status'] = 'attack'
+                if x-1>-1:
+                    if m.PI.Game.cells[x-1][y-1]['value'][:5] == "white":
+                        m.PI.Game.cells[x-1][y-1]['status'] = 'attack'
+
 class Game:
 
     def __init__(self,m):
+        
+        self.PM = PieseMove(m)
 
         self.cells = [
             [{"value":"white_rook" },{"value":"white_pawn" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"black_pawn" },{"value":"black_rook" }],
             [{"value":"white_knight" },{"value":"white_pawn" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"black_pawn" },{"value":"black_knight" }],
-            [{"value":"white_bishop" },{"value":"white_pawn" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"black_pawn" },{"value":"black_bishop" }],
+            [{"value":"white_bishop" },{"value":"white_pawn" },{"value":"black_pawn" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"black_pawn" },{"value":"black_bishop" }],
             [{"value":"white_queen" },{"value":"white_pawn" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"black_pawn" },{"value":"black_queen" }],
             [{"value":"white_king" },{"value":"white_pawn" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"black_pawn" },{"value":"black_king" }],
             [{"value":"white_bishop" },{"value":"white_pawn" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"empty" },{"value":"black_pawn" },{"value":"black_bishop" }],
@@ -25,36 +67,13 @@ class Game:
                 cell['status'] = "none"
         
         self.selected_cell = None
-        self.move = "white"
+        self.move = "black"
     
     def main(self,m):
 
         self.key_input(m)
         self.createpos(m)
-        
-        for x in range(8):
-            for y in range(8):
-
-                self.cells[x][y]['status'] = None
-                
-                if self.cells[x][y]['value'] != "empty" and self.cells[x][y]['value'][:5] == self.move:
-                    
-                    if m.PI.MI.mouse_click['lt']:
-                        
-                        if self.is_point_in_polygon(m.PI.MI.mouse_pos,self.cells[x][y]['points']):
-                            
-                            self.selected_cell = [x,y]
-                            
-        if self.selected_cell:
-            
-            cell = self.cells[self.selected_cell[0]][self.selected_cell[1]]
-            x = self.selected_cell[0]
-            y = self.selected_cell[1]
-            
-            cell['status'] = 'selected'
-            
-            if cell['value'][6:] == "pawn":
-                self.cells[x][y+1] = 'move'
+        self.mouse_input(m)
 
     def createpos(self,m):
 
@@ -93,6 +112,28 @@ class Game:
             
             if m.Disp.Game.rotate[i] < -180:
                 m.Disp.Game.rotate[i] += 360
+    
+    def mouse_input(self,m):
+        
+        for x in range(8):
+            for y in range(8):
+
+                self.cells[x][y]['status'] = None
+                
+                if self.cells[x][y]['value'] != "empty" and self.cells[x][y]['value'][:5] == self.move:
+                    
+                    if m.PI.MI.mouse_click['lt']:
+                        
+                        if self.is_point_in_polygon(m.PI.MI.mouse_pos,self.cells[x][y]['points']):
+                            
+                            self.selected_cell = [x,y]
+                            
+        if self.selected_cell:
+            
+            x,y = self.selected_cell
+            self.cells[x][y]['status'] = "selected"
+            
+            self.PM.main(m,x,y)
     
     def is_point_in_polygon(self,point:tuple,polygon:list) -> bool:
         
