8ed93c3c9f5adcceeb262d456b47ad9458503cd4
Svyatoslav <tortik2000.g@gmail.com>
2025-09-05 22:14:13 +0300

    1.0.3

--- scenes/chess/Display.py
+++ scenes/chess/Display.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 class Display:
 
@@ -7,4 +8,10 @@ class Display:
 
     def main(self,m):
 
-        m.Disp.Screen.fill((255,255,255))
+        m.Disp.Screen.fill((0,0,0))
+
+        dots = []
+
+        for i in range(4):
+            dots.append((math.cos(math.pi/2*i)*50+m.Disp.width//2,math.sin(math.pi/2*i)*50+m.Disp.height//2))
+            pygame.draw.circle(m.Disp.Screen,(255,255,255),dots[-1],5,1)
