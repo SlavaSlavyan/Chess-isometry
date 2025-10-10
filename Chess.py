73911a5aea22d4eac442c15bed13cb4837f0e4c6
Svyatoslav <tortik2000.g@gmail.com>
2025-10-10 15:34:35 +0300

    2.0.2

--- Chess.py
+++ Chess.py
@@ -1,9 +1,36 @@
-# Файл запуска.
+VERSION = "2.0.2 DEV"
+# Файл запуска
 
-from src.Main import Main # Импорт ГЛАВНОГО класса программы.
+error_log = []
 
-Chess = Main("vers: 2.0.1") # Экземпляр основного класса.
+try:
+    from multiprocessing import Process
+    from pathlib import Path
+    
+except: 
+    start_allow = False
+    error_log.append("P0")
+    # Если библиотеки python не удаётся загрузить, программа не даст разрешение на запуск.
 
-# Основной цикл.
-#while True:
-Chess.main()
+# Импорт ГЛАВНОГО класса программы.
+try:
+    from src.Main import Main
+    start_allow = True
+except: 
+    error_log.append("M0")
+    start_allow = False 
+    # Если главный модуль не удаётся загрузить, программа не даст разрешение на запуск.
+
+
+if start_allow:
+    
+    class ChessStart:
+        
+        def __init__(self):
+            
+            self.Chess = Main() # Экземпляр основного класса.
+        
+        def main(self):
+
+            # Основной цикл.
+            self.Chess.start()
--- src/GL.py
+++ src/GL.py
@@ -1,3 +1,7 @@
+# Этот класс объединяет весь OpenGL в один класс
+# По какой то причине я не могу это сделать в главном классе...
+# Живите с этим ¯\_(ツ)_/¯
+
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 #from OpenGL.GLU import *
